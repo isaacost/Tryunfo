699e501478cdf1cd1bb009b105a65815104c0009
Isa Costa <isa-hcosta@hotmail.com>
2022-08-24 16:10:29 -0300

    requisito 5

--- src/App.js
+++ src/App.js
@@ -10,9 +10,36 @@ class App extends React.Component {
     cardAttr2: '',
     cardAttr3: '',
     cardImage: '',
-    cardRare: '',
+    cardRare: 'normal',
     cardTrunfo: false,
-    isSaveButtonDisabled: false,
+    isSaveButtonDisabled: true,
+  };
+
+  validationSaveButton = () => {
+    const maxValue = 210;
+    const maxAtt = 90;
+    const zero = 0;
+
+    const { cardName,
+      cardDescription,
+      cardImage,
+      cardRare,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3 } = this.state;
+
+    const notEmpty = cardName.length
+      && cardDescription
+      && cardImage
+      && cardRare;
+    const sumAtt = Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= maxValue;
+    const valueAtt = Number(cardAttr1) <= maxAtt
+      && Number(cardAttr2) <= maxAtt
+      && Number(cardAttr3) <= maxAtt;
+    const positive = Number(cardAttr1) >= zero
+      && Number(cardAttr2) >= zero
+      && Number(cardAttr3) >= zero;
+    return notEmpty && sumAtt && valueAtt && positive;
   };
 
   handleChange = ({ target }) => {
@@ -20,6 +47,16 @@ class App extends React.Component {
     const value = (target.type === 'checkbox') ? target.checked : target.value;
     this.setState({
       [name]: value,
+    }, () => {
+      if (this.validationSaveButton()) {
+        this.setState({
+          isSaveButtonDisabled: false,
+        });
+      } else {
+        this.setState({
+          isSaveButtonDisabled: true,
+        });
+      }
     });
   };
 
